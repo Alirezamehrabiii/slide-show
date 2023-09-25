bda88f701b1cac4fed02c98da3908aa2c9df76c7
ali.mehrabii1998@gmail.com <Alirezamehrabiii>
2023-09-25 04:09:11 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// const slide = document.querySelectorAll('.slide');
+
+// setInterval(()=>{
+//    slide[0].classList.toggle('opacity');
+// },2000)
+
+// setInterval(()=>{
+//     slide[1].classList.toggle('opacity');
+//  },2000)
+ 
+//  setInterval(()=>{
+//     slide[2].classList.toggle('opacity');
+//  },3000)
+ 
+ 
